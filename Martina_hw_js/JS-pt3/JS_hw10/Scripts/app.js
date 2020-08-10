ad11ba3c8a53ae6ac8456125d88ce1a660b9d55a
Martina <martina.talevska@hotmail.com>
2020-08-10 16:09:21 +0200

    Quick fix

--- Martina_hw_js/JS-pt3/JS_hw10/Scripts/app.js
+++ Martina_hw_js/JS-pt3/JS_hw10/Scripts/app.js
@@ -3,16 +3,21 @@ let lastName = ["Butler", "Downey", "Jolie"];
 let orderNum =["1", "2", "3"];
 let result = [];
 
-function actors(num, first, last) {
+function FullNameofActors(num, first, last) {
     for (i = 0; i < orderNum.length; i++){
       result[i] = orderNum[i] + "." + firstName[i] + " " + lastName[i];
   }
 }
-actors (orderNum, firstName, lastName);
+FullNameofActors (orderNum, firstName, lastName);
 
 for (i = 0; i < result.length; i++) {
 }
-console.log(result[i]);
 
+function printFullName() {
+  console.log(result.map(FullNameofActors));
+
+}
+
+printFullName();
 
 
