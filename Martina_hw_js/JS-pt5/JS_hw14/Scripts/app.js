e5ecd86a9309a3edc56f3bd2c1157208936635dc
Martina <martina.talevska@hotmail.com>
2020-08-17 03:56:02 +0200

    Add files via upload

---
+++ Martina_hw_js/JS-pt5/JS_hw14/Scripts/app.js
@@ -0,0 +1,24 @@
+let formBtn = document.getElementById("submitBtn");
+let formResult = document.getElementById("formInfo");
+
+formBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    console.log(event.target.value);
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let pswd = document.getElementById("pswd").value;
+    
+
+    formResult.innerHTML = "";
+
+    if (firstName === "" || lastName === "" || email === "" || pswd === "") {
+        formResult.style.color = "red";
+        formResult.innerHTML += `<p>Invalid input, please enter some value in the field!</p>`;
+    } else {
+        formResult.style.color = "black";
+        formResult.innerHTML += `<p>Hello ${firstName} ${lastName}, your e-mail is ${email} </p>`;
+    }
+});
